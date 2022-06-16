bb54808a3dc55aa02613f6b8ec15a7ec72f4a56c
asinaga117 <asinaga1994@gmail.com>
2022-06-16 10:51:42 +0000

    [ADD]在庫詳細画面追加

--- app/controllers/admin/inventryes_controller.rb
+++ app/controllers/admin/inventryes_controller.rb
@@ -2,14 +2,27 @@ class Admin::InventryesController < ApplicationController
   def new
     @inventry = Inventry.new
   end
+  
+  def create
+    @inventry = Inventry.new(inventry_params)
+    @inventry.save
+    redirect_to admin_inventryes_path(@inventry.id)
+  end
 
   def index
     @inventryes = Inventry.all
   end
 
   def show
+    @inventry = Inventry.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  
+  def inventry_params
+    params.require(:inventry).permit(:image,:name,:place,:is_active,:introduction,:amount)
   end
 end
--- app/models/inventry.rb
+++ app/models/inventry.rb
@@ -1,3 +1,3 @@
 class Inventry < ApplicationRecord
-  
+  has_one_attached :image
 end
